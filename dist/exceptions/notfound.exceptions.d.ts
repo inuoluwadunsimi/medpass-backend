da44ca613aec1a7961360277c2c92f2da617bbe3
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2024-06-13 21:14:02 +0100

    chore: untrack dist

--- dist/exceptions/notfound.exceptions.d.ts
+++
@@ -1,4 +0,0 @@
-import { ExceptionFilter, HttpException, ArgumentsHost } from '@nestjs/common';
-export declare class NotFoundExceptionFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost): void;
-}
--- dist/exceptions/validations.exceptions.d.ts
+++
@@ -1,5 +0,0 @@
-import { BadRequestException, ValidationError } from '@nestjs/common';
-export default class ValidationExceptions extends BadRequestException {
-    validationErrors: ValidationError[];
-    constructor(validationErrors: ValidationError[]);
-}
