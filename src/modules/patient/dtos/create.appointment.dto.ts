74faf1130eeb90ab8addcccfa905fc661d7c7ff5
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2024-06-17 14:55:38 +0100

    feat: update appointment dto

--- src/modules/patient/dtos/create.appointment.dto.ts
+++ src/modules/patient/dtos/create.appointment.dto.ts
@@ -7,7 +7,42 @@ import {
   IsOptional,
   IsString,
   MinLength,
+  ValidateNested,
 } from "class-validator";
+
+export class TreatmentDto {
+  @ApiProperty({ example: "aspirin" })
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @ApiProperty({ example: "aspirin" })
+  @IsString()
+  @IsOptional()
+  description: string;
+}
+
+export class PrescriptionDto {
+  @ApiProperty({ example: "aspirin" })
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @ApiProperty({ example: "95mg" })
+  @IsString()
+  @IsOptional()
+  mg: string;
+
+  @ApiProperty({ example: "aspirin f" })
+  @IsString()
+  @IsOptional()
+  measurement: string;
+
+  @ApiProperty({ example: "29 times daily" })
+  @IsString()
+  @IsOptional()
+  frequency: string;
+}
 
 export class CreateAppointmentDto {
   @ApiProperty({ example: "2021-09-01T00:00:00.000Z" })
@@ -23,4 +58,12 @@ export class CreateAppointmentDto {
   doctorsReport: string[];
 }
 
-export class UpdateAppointmentDto extends CreateAppointmentDto {}
+export class UpdateAppointmentDto extends CreateAppointmentDto {
+  @ApiProperty()
+  @ValidateNested()
+  treatment: TreatmentDto;
+
+  @ApiProperty()
+  @ValidateNested()
+  prescription: PrescriptionDto;
+}
