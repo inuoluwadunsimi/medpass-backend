bc287251e8550d7f34536d5f1d3269379fe11636
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2024-06-18 11:51:45 +0100

    get user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -13,7 +13,37 @@ import {
   UseInterceptors,
 } from "@nestjs/common";
 
-import { ApiConsumes, ApiOperation, ApiTags } from "@nestjs/swagger";
+import {
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from "@nestjs/swagger";
+import { AppAuthGuard } from "../auth/guards/auth.guard";
+import * as ResponseManager from "../../helpers/response.helpers";
+import { UserService } from "./user.service";
+import { User } from "./schemas";
+import { Response } from "express";
+import { IExpressRequest } from "../auth/jwt/jwt.interface";
 @Controller("user")
 @ApiTags("user")
-export class UserController {}
+@UseGuards(AppAuthGuard)
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get("/me")
+  @ApiResponse({ status: 200, type: User })
+  public async getUserProfile(
+    @Res() res: Response,
+    @Req() req: IExpressRequest
+  ): Promise<void> {
+    const user = req.userId;
+    try {
+      const data = await this.userService.getUserProfile(user);
+      ResponseManager.success(res, { data });
+      ``;
+    } catch (err: any) {
+      ResponseManager.handleError(res, err);
+    }
+  }
+}
